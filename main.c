93fd326be881a1b331779971a43dc242cdd2a801
pabloRnt <pab.rena15@gmail.com>
2026-03-20 09:06:06 -0300

    Finalizacao do projeto

--- main.c
+++ main.c
@@ -6,5 +6,7 @@
     // Nome de um integrante sendo adicionado:
     printf("Tiago Oliveira Ayala");
     printf("Pablo Renato dos Santos Sobral de Carvalho");
+
+    printf("Nosso grupo de garotos de programa: Thyago, Lucas e Pablo")
     return 0;
  }
