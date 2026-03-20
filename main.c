62b6acd040fead25716a1af45d3595a6be7ea836
tiagooayala <tiagooayala@gmail.com>
2026-03-20 08:54:59 -0300

    Adicionar quebra linha

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
  int main(void){
   
-    printf("Lucas Barreto Santana");
+    printf("Lucas Barreto Santana \n");
     // Nome de um integrante sendo adicionado:
     printf("Tiago Oliveira Ayala");
     return 0;
